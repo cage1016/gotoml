21fcb3c7938689ba62512ab6652654cdb63eba5e
Kai-Chu Chung <cage.chung@gmail.com>
2017-09-12 14:11:41 +0800

    test(Conf): add test case

--- example/uLinux.go
+++ example/uLinux.go
@@ -3,51 +3,63 @@ package example
 import ()
 
 type Example struct {
-	System system `mapstructure:"System"`
-	XXX    xxx    `mapstructure:"XXX"`
+	DDNS              ddns              `mapstructure:"DDNS"`
+	END_FLAG          end_flag          `mapstructure:"END_FLAG"`
+	NetDevMonitor     netdevmonitor     `mapstructure:"NetDevMonitor"`
+	Network_Group     network_group     `mapstructure:"Network_Group"`
+	QDK               qdk               `mapstructure:"QDK"`
+	System            system            `mapstructure:"System"`
+	Container_station container_station `mapstructure:"container_station"`
+	ISCSI             iscsi             `mapstructure:"iSCSI"`
+}
+
+type container_station struct {
+	Class  string `mapstructure:"Class"`
+	Name   string `mapstructure:"Name"`
+	Status string `mapstructure:"Status"`
+}
+
+type ddns struct {
+	Check_External_IP int64  `mapstructure:"Check_External_IP"`
+	Enable            bool   `mapstructure:"Enable"`
+	IP_Address        string `mapstructure:"IP_Address"`
+	Server_Type       int64  `mapstructure:"Server_Type"`
+	User_Name         string `mapstructure:"User_Name"`
+	EPassword         string `mapstructure:"ePassword"`
+}
+
+type end_flag struct {
+}
+
+type iscsi struct {
+	Company_Info string `mapstructure:"Company_Info"`
+	Model_Name   string `mapstructure:"Model_Name"`
+	Uni_String   string `mapstructure:"Uni_String"`
+}
+
+type netdevmonitor struct {
+	Current_bitmap string `mapstructure:"current_bitmap"`
+	Version        int64  `mapstructure:"version"`
+}
+
+type network_group struct {
+	QPHOTOSTATION_LOGO bool `mapstructure:"QPHOTOSTATION_LOGO"`
+	Qdownlod_LOGO      bool `mapstructure:"Qdownlod_LOGO"`
+}
+
+type qdk struct {
+	Build                    int64  `mapstructure:"Build"`
+	Enable                   bool   `mapstructure:"Enable"`
+	Install_Path             string `mapstructure:"Install_Path"`
+	RC_Number                int64  `mapstructure:"RC_Number"`
+	Cfg__etc_config_qdk_conf int64  `mapstructure:"cfg__etc_config_qdk_conf"`
 }
 
 type system struct {
-	AutoCreateRaid           string `mapstructure:"AutoCreateRaid"`
-	BuildNumber              int64  `mapstructure:"BuildNumber"`
-	CodePage                 int64  `mapstructure:"CodePage"`
-	E2KEYSupport             bool   `mapstructure:"E2KEYSupport"`
-	EnableDaylightSavingTime bool   `mapstructure:"EnableDaylightSavingTime"`
-	FSType                   string `mapstructure:"FSType"`
-	InternalModel            string `mapstructure:"InternalModel"`
-	LanAccess                bool   `mapstructure:"LanAccess"`
-	Model                    string `mapstructure:"Model"`
-	RsyncModel               string `mapstructure:"RsyncModel"`
-	RsyncSupport             bool   `mapstructure:"RsyncSupport"`
-	ServerComment            string `mapstructure:"ServerComment"`
-	SystemDevice             string `mapstructure:"SystemDevice"`
-	TestMode                 bool   `mapstructure:"TestMode"`
-	TimeZone                 string `mapstructure:"TimeZone"`
-	Version                  string `mapstructure:"Version"`
-	WanAccess                bool   `mapstructure:"WanAccess"`
-	WebAccessPort            int64  `mapstructure:"WebAccessPort"`
+	Latest_Check_Live_Update string `mapstructure:"Latest_Check_Live_Update"`
+	LoginTheme410            int64  `mapstructure:"LoginTheme410"`
+	Qphoto_LOGO              bool   `mapstructure:"Qphoto_LOGO"`
+	Server_Name              string `mapstructure:"Server_Name"`
+	UPNP_UUID                string `mapstructure:"UPNP_UUID"`
 	Workgroup                string `mapstructure:"Workgroup"`
-}
-
-type xxx struct {
-	AutoPowerOn           bool   `mapstructure:"AutoPowerOn"`
-	Booting               int64  `mapstructure:"Booting"`
-	BuildDate             string `mapstructure:"BuildDate"`
-	DateFormatIndex       int64  `mapstructure:"DateFormatIndex"`
-	EnableLiveUpdate      bool   `mapstructure:"EnableLiveUpdate"`
-	ExtPort               int64  `mapstructure:"ExtPort"`
-	ForceSSL              int64  `mapstructure:"ForceSSL"`
-	LatestCheckLiveUpdate string `mapstructure:"LatestCheckLiveUpdate"`
-	LatestLiveUpdate      string `mapstructure:"LatestLiveUpdate"`
-	LoginBgNum            int64  `mapstructure:"LoginBgNum"`
-	LoginTheme410         int64  `mapstructure:"LoginTheme410"`
-	Passwordconstraints   int64  `mapstructure:"Passwordconstraints"`
-	Qsirch                int64  `mapstructure:"Qsirch"`
-	QuotaType             string `mapstructure:"QuotaType"`
-	ServerName            string `mapstructure:"ServerName"`
-	UPNP_UUID             string `mapstructure:"UPNP_UUID"`
-	UpdateRemindTime      int64  `mapstructure:"UpdateRemindTime"`
-	VM                    int64  `mapstructure:"VM"`
-	WriteConnectionLog    int64  `mapstructure:"WriteConnectionLog"`
-	Agree_beta            int64  `mapstructure:"agree_beta"`
 }
